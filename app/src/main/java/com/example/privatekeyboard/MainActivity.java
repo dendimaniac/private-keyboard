8da5f23cf6d3e1510ff2c26b5824a612ec144afc
Ren <maithieunhan@gmail.com>
2020-11-19 12:34:59 +0200

    save data after take pic

--- app/src/main/java/com/example/privatekeyboard/MainActivity.java
+++ app/src/main/java/com/example/privatekeyboard/MainActivity.java
@@ -191,18 +191,6 @@ public class MainActivity extends AppCompatActivity {
         Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        // Save UI state changes to the savedInstanceState.
-        // This bundle will be passed to onCreate if the process is
-        // killed and restarted.
-        savedInstanceState.putBoolean("MyBoolean", true);
-        savedInstanceState.putDouble("myDouble", 1.9);
-        savedInstanceState.putInt("MyInt", 1);
-        savedInstanceState.putString("MyString", "Welcome back to Android");
-        // etc.
-    }
 
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
