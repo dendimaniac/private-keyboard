6f586695418b24a8950610abb6e5e813761a7dd3
Nhan Mai <maithieunhan@gmail.com>
2020-12-09 19:38:04 +0200

    Update EmailConfig.java

--- app/src/main/java/com/example/privatekeyboard/Data/EmailConfig.java
+++ app/src/main/java/com/example/privatekeyboard/Data/EmailConfig.java
@@ -3,7 +3,7 @@ package com.example.privatekeyboard.Data;
 import java.util.HashMap;
 
 public class EmailConfig {
-    public static final String EMAIL ="maithieunhan@gmail.com";
+    public static final String EMAIL ="noreply.privatekeyboard@gmail.com";
     public static final String PASSWORD ="tiowinpbxqsckjqh";
     public static HashMap<String,String> saveInstance = new HashMap<>();
 
